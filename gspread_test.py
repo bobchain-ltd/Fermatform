e3b3332ce3fa9595915dbf16a3f1e9196b8d251d
Levente Szabados <levente.szabados@analogy.co>
2017-04-05 21:58:20 +0200

    first form ready - untested

--- gspread_test.py
+++ gspread_test.py
@@ -7,16 +7,15 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('f.json', scope)
 
 gc = gspread.authorize(credentials)
 
-print gc
-
-#gc.openall()#open_by_url("https://docs.google.com/spreadsheets/d/18qWEJe7UTW51-mGv6v1CZcIt5Nt0u1phnLKUw9XQ25c/edit#gid=0")#open_by_key("18qWEJe7UTW51-mGv6v1CZcIt5Nt0u1phnLKUw9XQ25c")#("Checkins").sheet1
 wks = gc.open("Checkins")
-sheet = wks.worksheet("Done tasks")
-sheet.resize(rows=1,cols=4)
+done_tasks = wks.worksheet("Done tasks")
+days = wks.worksheet("Days")
+discussion_requests = wks.worksheet("Discussion requests")
 
-sheet2 = wks.worksheet("Days")
-sheet2.resize(rows=1,cols=3)
+#WARN: Do NOT comment this back, since it kills the spreadsheet content!
+#days.resize(rows=1,cols=3)
+#discussion_requests.resize(rows=1,cols=4)
+#done_tasks.resize(rows=1,cols=4)
 
-sheet2 = wks.worksheet("Discussion requests")
-sheet2.resize(rows=1,cols=4)
+
 #sheet.append_row(["a","b","c"])
--- multimodel.py
+++ multimodel.py
@@ -6,16 +6,20 @@ from wtforms import Form as NoCsrfForm
 from wtforms.fields import StringField, FormField, SubmitField, IntegerField, SelectField
 from wtforms.validators import DataRequired, NumberRange
 from datetime import date, timedelta
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 SECRET_KEY = 'a really not so random string'
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
+CURRENT_DATE = date.today().strftime('%m-%d-%Y')
+YESTERDAY = (date.today()-timedelta(1)).strftime('%m-%d-%Y')
+
+OPTIONS = {"Very bad":-2,"No good":-1,"OK":0,"Above expected":1,"Supercool":2}
 
 class Checkin():
-    current_date = date.today().strftime('%m-%d-%Y')
-    yesterday = (date.today()-timedelta(1)).strftime('%m-%d-%Y')
     dev_name = ""
     tasks = ""
     evaluation = ""
@@ -43,15 +47,45 @@ class PlanForm(NoCsrfForm):
 class CombinedForm(FlaskForm):
     dev_name = StringField('My name / nick', validators=[DataRequired()])
     tasks = FieldList(FormField(TaskForm, default=lambda: Task()))
-
-    options = ["Very bad","No good","OK","Above expected","Supercool"]
-    evaluation = SelectField('Self evaluation', choices = [(o, o) for o in options] , validators = [DataRequired()])
+    
+    evaluation = SelectField('Self evaluation', choices = [(o, o) for o in OPTIONS.keys()] , validators = [DataRequired()])
 
     plans = FieldList(FormField(PlanForm, default=lambda: Plan()))
 
     submit = SubmitField('Submit')
 
+def save_the_day(form):
+    days = wks.worksheet("Days")
+    row=[YESTERDAY,form.dev_name.data,OPTIONS[form.evaluation.data]]
+    days.append_row(row)
+    return
+
+def save_done_tasks(form):
+    done_tasks = wks.worksheet("Done tasks")
+
+    for  task in form.tasks.data:
+        row=[YESTERDAY,form.dev_name.data,task["task_name"],task["duration"]]
+        done_tasks.append_row(row)
+    return
+
+def save_discussion_requests(form):
+    discussion_requests = wks.worksheet("Discussion requests")
+    for req in form.contacts.data:
+        if "," in req["contacts"]: 
+            splitted=req["contacts"].split(",")
+        elif " " in req["contacts"]:
+            splitted=req["contacts"].split(" ")
+        else:
+            splitted=req
+
+        for individual in splitted:
+            row=[CURRENT_DATE,form.dev_name.data,req.plan_name.data,individual]
+            discussion_requests.append_row(row)
+    return
+
 def save_to_google(form):
+    #save_the_day(form)
+    #save_done_tasks(form)
     return
 
 @app.route('/thanks', methods=['GET',])
@@ -72,13 +106,22 @@ def index():
 
     if form.validate_on_submit():
         form.populate_obj(checkin)
-        flash("Saved Changes")
+
         save_to_google(form)
+
         return redirect(url_for('thanks'))
 
     return render_template('multi.html', form=form)
 
 
-
 if __name__ == '__main__':
+
+    scope = ['https://spreadsheets.google.com/feeds']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('f.json', scope)
+
+    gc = gspread.authorize(credentials)
+
+    wks = gc.open("Checkins")
+
     app.run(debug=True)#, port=5000)
