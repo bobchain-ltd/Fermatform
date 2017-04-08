da0f0aca8f868762a35c3d7330b6a4aeaba811de
Levente Szabados <levente.szabados@analogy.co>
2017-04-08 09:28:23 +0200

    remove token

--- slack-test.py
+++ slack-test.py
@@ -2,9 +2,6 @@ from slackclient import SlackClient
 
 with open('slack.auth', 'r') as f:
 	token=f.read().rstrip()
-print token
-
-#token="xoxp-145472380709-146388452275-166445283399-d06496f8ed102be9bb858bce7f86c0e8"
 sc = SlackClient(token)
 
 channels = sc.api_call(
